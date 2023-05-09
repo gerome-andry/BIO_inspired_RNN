643842a4f39c8bb3fe619d8552a6dc8fc8c605d4
Andry Gerome <andry.gerome@gmail.com>
2023-05-09 12:07:52 +0200

    let's cry

--- BIRNN.py
+++ BIRNN.py
@@ -4,24 +4,24 @@ import torch.nn as nn
 def encode_choice(probs):
     # probs of size (B, L, 3)
     choice = torch.argmax(probs, dim = -1)
-    choice[choice == 2] = -1 # -> (B,L)
+    # choice[choice == 2] = -1 # -> (B,L)
     
     return choice
 
 def decode_choice(choice):
-    probs = torch.zeros(choice.shape + (3,))
+    probs = torch.zeros(choice.shape + (2,))
     
-    m0 = (choice == 0)[...,None].repeat(1,1,3)
-    m0[...,[1,2]] = False
+    m0 = (choice == 0)[...,None].repeat(1,1,2)
+    m0[...,[1]] = False
     probs[m0] = 1
 
-    m1 = (choice == 1)[...,None].repeat(1,1,3)
-    m1[...,[0,2]] = False
+    m1 = (choice == 1)[...,None].repeat(1,1,2)
+    m1[...,[0]] = False
     probs[m1] = 1
 
-    m2 = (choice == -1)[...,None].repeat(1,1,3)
-    m2[...,[0,1]] = False
-    probs[m2] = 1
+    # m2 = (choice == -1)[...,None].repeat(1,1,3)
+    # m2[...,[0,1]] = False
+    # probs[m2] = 1
 
     return probs
 
@@ -42,11 +42,13 @@ class ResMLP(nn.Module):
     
 
 class SenseMemAct(nn.Module):
-    def __init__(self, sensor_net, actor_net, mem_lay = 1, in_sz = 1, mem_sz = 64, bias = False):
+    def __init__(self, sensor_net, actor_net, mem_lay = 1, in_sz = 1, mem_sz = 64, decisions = 3, bias = False):
         super().__init__()
         self.sense = sensor_net
         self.act = actor_net
-        self.mem = nn.GRU(in_sz, mem_sz, mem_lay, bias = bias, batch_first = True)
+        self.dec = decisions
+        self.mem = [nn.GRU(in_sz, mem_sz, mem_lay, bias = bias, batch_first = True)]
+        # self.mem = nn.ModuleList([nn.GRU(in_sz, mem_sz, 1, bias = bias, batch_first = True) for _ in range(mem_lay)])
         self.decision = nn.Softmax(dim = -1)
         self.l = nn.CrossEntropyLoss()
 
@@ -57,18 +59,41 @@ class SenseMemAct(nn.Module):
         # transfer sequence to sensor -> go back to sequence
         inputs = self.sense(x.reshape((-1, N))).reshape((B,L,-1))
         # transfer to memory
-        memory,_ = self.mem(inputs)
+        if len(self.mem) > 1:
+            memory = []
+            mems = []
+            for l in range(L):
+                for i,m in enumerate(self.mem):
+                    if l ==0 and i == 0:
+                        hi,_ = m(inputs[:,l,:].unsqueeze(1))
+                    else:
+                        hi,_ = m(inputs[:,l,:].unsqueeze(1), mems[-1])
+                        if i == 0:
+                            mems = []
+
+                    mems.append(hi.transpose(0,1))
+                memory.append(torch.cat([m.transpose(0,1) for m in mems], dim = 2))
+
+            memory = torch.cat(memory, dim = 1) #retrieve the sequence of hidden states
+        else:
+            memory,_ = self.mem[0](inputs)
+
         B, L, M = memory.shape
         # transfer sequence output to actions sequence 
-        out = self.decision(self.act(memory.reshape((-1, M))).reshape((B,L,3)))
+        
+        out = self.decision(self.act(memory.reshape((-1, M))).reshape((B,L,self.dec)))
+        # out = self.decision(memory[:,:,:3])
 
         return out
 
     def loss(self, x, target):
         # X - (B,L,N) | T - (B,L,O), O = 3 (choices)
-        pred = self(x)
 
-        return  self.l(pred, target)
+        mask = (target[:,:,0] != 1)
+        pred = self(x)
+        
+        not_m = torch.bitwise_not(mask)
+        return  self.l(pred, target)#2*self.l(pred[mask], target[mask])# + .5*self.l(pred[not_m], target[not_m])
 
 # probs = torch.rand((1,10,3))
 # print(probs)
--- train.py
+++ train.py
@@ -10,14 +10,15 @@ os.environ["KMP_DUPLICATE_LIB_OK"] = "True"
 epoch = 64
 batch = 64
 batch_sz = 128
-memory_size = 64
+memory_size = 32
 in_emb = memory_size//2
-decisions = 3
+mem_lay = 5
+decisions = 2
 
 sensor = ResMLP(1, in_emb, [64,64,64])
 actor = ResMLP(memory_size, decisions, [64,64,64])
 
-model = SenseMemAct(sensor, actor, in_sz=in_emb, mem_sz=memory_size, mem_lay=2)
+model = SenseMemAct(sensor, actor, in_sz=in_emb, mem_sz=memory_size, mem_lay=mem_lay, decisions=decisions)
 
 sg = StimGenerator(dt = .1)
 optimizer = torch.optim.AdamW(
@@ -25,6 +26,12 @@ optimizer = torch.optim.AdamW(
             lr=1e-3
         )
 # Training loop
+
+choice_d = {
+    0 : 'none',
+    1 : 'same',
+    2 : 'diff'
+}
 
 loss = []
 for ep in trange(epoch):
@@ -40,23 +47,27 @@ for ep in trange(epoch):
         with torch.autograd.set_detect_anomaly(True):
             l.backward()
             optimizer.step()
-        # print(l)
+        print(l)
         loss.append(l.detach())
-        # with torch.no_grad():
-        #     inp, out = sg.get_batch_data(2)
-        #     inp, out = sg.extend_sim(30, inp, out)
-        #     # inp += torch.randn_like(inp)*.01
-        #     # out += torch.randn_like(out)*.01
-        #     # out *= 0
-        #     pred = model(inp.unsqueeze(-1))
-        #     pred = encode_choice(pred)
-        #     plt.plot(inp[0,:])
-        #     plt.title('pred')
-        #     plt.plot(out[0,:])
-        #     plt.plot(pred[0,:])
-        #     plt.show(block = False)
-        #     plt.pause(.01)
-        #     plt.clf()
+        with torch.no_grad():
+            inp, out = sg.get_batch_data(2)
+            inp, out = sg.extend_sim(30, inp, out)
+            # inp += torch.randn_like(inp)*.01
+            # out += torch.randn_like(out)*.01
+            # out *= 0
+            pred = model(inp.unsqueeze(-1))
+
+            for i in range(2):
+                plt.plot(pred[0,:,i], label = f'p({choice_d[i]})')
+
+            pred = encode_choice(pred)
+            plt.plot(inp[0,:])
+            plt.title('pred')
+            plt.plot(out[0,:])
+            plt.plot(pred[0,:])
+            plt.show(block = False)
+            plt.pause(.01)
+            plt.clf()
 
 plt.plot(loss)
 plt.show()
